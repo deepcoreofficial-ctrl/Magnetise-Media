683eb7c0587002f4dea5e0f7b1719b5e29a21264
Daksh Yadav <deepcoreofficial@gmail.com>
2026-05-08 23:43:59 +0530

    fix db env

--- app.py
+++ app.py
@@ -35,6 +35,7 @@ limiter = Limiter(
 
 # MySQL config
 app.config['MYSQL_HOST'] = os.getenv('MYSQL_HOST')
+app.config['MYSQL_PORT'] = int(os.getenv('MYSQL_PORT'))
 app.config['MYSQL_USER'] = os.getenv('MYSQL_USER')
 app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD')
 app.config['MYSQL_DB'] = os.getenv('MYSQL_DB')
